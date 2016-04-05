295ca116e69eee1cc28d06b5f8dd97944647c82f
Peter Freschi <pfreschi@uw.edu>
2016-04-05 12:25:25 -0700

    finished simple and complex operations in calculator

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -7,6 +7,57 @@
 //
 
 import Foundation
+print("Enter an expression separated by returns or write a multi-operand operation:")
+let terminators = ["count", "avg", "fact"]
+let operations = ["+", "-", "*", "/", "%"]
+var count = 0
+var sum = UInt.init()
+var response = readLine(stripNewline: true)!
+let numbers = response.componentsSeparatedByString(" ")
+if (!response.containsString(" ")) {
+        var current = UInt.init(response)!
+        let operation = readLine(stripNewline: true)!
+        let secondInput = readLine(stripNewline: true)!
+        var second = UInt.init(secondInput)!
+        switch operation {
+            case "+":
+                current+=second
+            case "-":
+                current-=second
+            case "*":
+                current*=second
+            case "/":
+                current/=second
+            case "%":
+                current%=second
+            default:
+                print("invalid operation")
+        }
+        print("Result: \(current)");
+        sum+=UInt.init(response)!
+    
+} else if (response.containsString("count")){
+    for number in numbers {
+        if (number != "count") {
+            count += 1;
+        }
+    }
+    print("Count: \(count)")
+    
+} else if (response.containsString("avg")){
+    for number in numbers {
+        if (number != "avg") {
+            count += 1;
+            sum += UInt.init(number)!
+        }
+    }
+    print("Average: \(sum/UInt(count))")
 
-print("Hello, World!")
-
+} else if (response.containsString("fact")){
+    var number = UInt.init(numbers[0])
+    var result = UInt.init(1);
+    for index in 1...Int(number!) {
+        result = result*UInt(index)
+    }
+    print("Factorial: \(result)")
+}
