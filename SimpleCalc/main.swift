742be31f6523abdfa4151124ceb61c58450f1884
Peter Freschi <pfreschi@uw.edu>
2016-04-04 22:19:28 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Peter Freschi on 4/4/16.
+//  Copyright © 2016 Peter Freschi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
